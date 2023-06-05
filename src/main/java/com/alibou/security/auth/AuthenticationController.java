7a7b632aab5b56ef9ba2e9a1a3e5691f021f4bfa
SardorbekJavaDev <sardorbekyorqulov1@gmail.com>
2023-06-05 08:17:51 +0500

    Initial commit

---
+++ src/main/java/com/alibou/security/auth/AuthenticationController.java
@@ -0,0 +1,49 @@
+package com.alibou.security.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        log.info("/api/v1/auth/register : {}{}", request, AuthenticationController.class);
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) {
+        log.info("/api/v1/auth/authenticate : {}{}", request, AuthenticationController.class);
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        log.info("/api/v1/auth/refresh-token : {}{}{}", request, response, AuthenticationController.class);
+        service.refreshToken(request, response);
+    }
+
+
+}
